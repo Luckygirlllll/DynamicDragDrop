772f1e47fb8f154ab92816c1dfecc7349be2ef10
Luckygirlllll <irynakanivets@gmail.com>
2016-07-04 12:49:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/attracti/dynamicdragdrop/CheeseDynamicAdapter.java
@@ -0,0 +1,53 @@
+package com.example.attracti.dynamicdragdrop;
+
+/**
+ * Created by Iryna on 7/4/16.
+ */
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.attracti.dynamicdragdrop.dynamicgrid.BaseDynamicGridAdapter;
+
+import java.util.List;
+
+
+public class CheeseDynamicAdapter extends BaseDynamicGridAdapter {
+    public CheeseDynamicAdapter(Context context, List<?> items, int columnCount) {
+        super(context, items, columnCount);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        CheeseViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_grid, null);
+            holder = new CheeseViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (CheeseViewHolder) convertView.getTag();
+        }
+        holder.build(getItem(position).toString());
+        return convertView;
+    }
+
+    private class CheeseViewHolder {
+        private TextView titleText;
+        private ImageView image;
+
+        private CheeseViewHolder(View view) {
+            titleText = (TextView) view.findViewById(R.id.item_title);
+            image = (ImageView) view.findViewById(R.id.item_img);
+        }
+
+        void build(String title) {
+            titleText.setText(title);
+            image.setImageResource(R.drawable.folder_multiple_image);
+        }
+    }
+}
